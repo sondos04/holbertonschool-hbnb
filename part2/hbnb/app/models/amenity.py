6ab443af42e2347dca25b18608433617a8a108a4
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-06 01:41:55 +0300

    Refactor indentation and formatting in Amenity class

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,24 +1,26 @@
 from app.models.BaseModel import BaseModel
 
+
 class Amenity(BaseModel):
-   """Represents an amenity in the HBnB."""
-  def __init__(self, name):
-    super().__init__()
-    
-    if not isinstance(name, str):
-      raise TypeError("name must be a string.")
-    name = name.strip()
-    if not name:
-      raise ValueError("amenity name is required.")
-    if len(name) > 50:
-      raise ValueError("amenity name must be at most 50 characters.")
-      
-    self.name = name
-    
-  def to_dict(self):
-    return {
-      "id" : self.id,
-      "name" : self.name,
-      "created_at": self.created_at.isoformat(),
-      "updated_at": self.updated_at.isoformat(),
-    }    
+    def __init__(self, name):
+        super().__init__()
+
+        if not isinstance(name, str):
+            raise TypeError("name must be a string.")
+
+        name = name.strip()
+        if not name:
+            raise ValueError("amenity name is required.")
+
+        if len(name) > 50:
+            raise ValueError("amenity name must be at most 50 characters.")
+
+        self.name = name
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
