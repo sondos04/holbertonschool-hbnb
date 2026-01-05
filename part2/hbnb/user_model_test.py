0d0e21e83f720add4245dd2f2513ed8e4bec8416
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-06 01:17:27 +0300

    Update user_model_test.py

--- part2/hbnb/user_model_test.py
+++ part2/hbnb/user_model_test.py
@@ -1,11 +1,11 @@
 from app.models.user import User
-from app.models.base_model import BaseModel
-def test_user():
-    user = User("sondos", "alrubaish", "sondos@example.com")
-    assert user.first_name == "sondos"
-    assert user.email == "sondos@example.com"
-    assert user.is_admin is False
-    print("User test passed")
 
-if __name__ == "__main__":
-    test_user()
+def test_user_creation():
+    user = User(first_name="John", last_name="Doe", email="john.doe@example.com")
+    assert user.first_name == "John"
+    assert user.last_name == "Doe"
+    assert user.email == "john.doe@example.com"
+    assert user.is_admin is False  # Default value
+    print("User creation test passed!")
+
+test_user_creation()
