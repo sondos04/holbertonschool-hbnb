6e26e135e3e1e7e1dcca08f029ca5d6644c3ed83
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-06 04:01:21 +0300

    Define abstract methods in Repository class

--- part2/hbnb/app/persistence/repository.py
+++ part2/hbnb/app/persistence/repository.py
@@ -1,3 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class Repository(ABC):
+    @abstractmethod
+    def add(self, obj):
+        pass
+
+    @abstractmethod
+    def get(self, obj_id):
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        pass
+
+    @abstractmethod
+    def update(self, obj_id, data):
+        pass
+
+    @abstractmethod
+    def delete(self, obj_id):
+        pass
+
+    @abstractmethod
+    def get_by_attribute(self, attr_name, attr_value):
+        pass
+
+
 class InMemoryRepository(Repository):
     def __init__(self):
         self._storage = {}
