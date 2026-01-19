a2576004b427c4c92f828753e0f157ce582b1841
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-19 14:21:31 +0300

    Remove AmenityRepository from HBnBFacade
    Comment out the import and initialization of AmenityRepository.

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -2,11 +2,11 @@ from app.db.session import SessionLocal
 from app.repositories.user_repository import UserRepository
 from app.repositories.place_repository import PlaceRepository
 from app.repositories.review_repository import ReviewRepository
-from app.repositories.amenity_repository import AmenityRepository
+#from app.repositories.amenity_repository import AmenityRepository
 from app.models.user import User
 from app.models.place import Place
 from app.models.review import Review
-from app.models.amenity import Amenity
+#from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -14,7 +14,7 @@ class HBnBFacade:
         self.user_repo = UserRepository(self.session)
         self.place_repo = PlaceRepository(self.session)
         self.review_repo = ReviewRepository(self.session)
-        self.amenity_repo = AmenityRepository(self.session)
+        #self.amenity_repo = AmenityRepository(self.session)
 
     # ======================
     # Users
