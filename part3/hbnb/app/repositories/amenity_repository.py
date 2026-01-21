8a5140d4c4b75fb7d18d17db76225b24f6e48752
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-21 10:15:09 +0300

    Implement AmenityRepository with get_by_name method

--- part3/hbnb/app/repositories/amenity_repository.py
+++ part3/hbnb/app/repositories/amenity_repository.py
@@ -1,1 +1,11 @@
+from app.models.amenity import Amenity
+from app.repositories.sqlalchemy_repository import SQLAlchemyRepository
 
+
+class AmenityRepository(SQLAlchemyRepository):
+    def __init__(self, session):
+        super().__init__(session, Amenity)
+
+    def get_by_name(self, name):
+        """Get amenity by name"""
+        return self.session.query(Amenity).filter_by(name=name).first()
