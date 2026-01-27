95179a6db039a41aa382471a2e3791ebaa258d3a
Nada Al-Mutairi <nada.ghannam19@gmail.com>
2026-01-27 16:57:03 +0300

    Refactor create_app function and update API initialization

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 
+from config import DevelopmentConfig
 from app.extensions import db, bcrypt, jwt
 
 from app.api.v1.users import api as users_ns
@@ -9,15 +10,22 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-def create_app(config_class="config.DevelopmentConfig"):
-    app = Flask(__name__)
+
+def create_app(config_class=DevelopmentConfig):
+    app = Flask(name)
     app.config.from_object(config_class)
 
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    api = Api(app, version="1.0", title="HBnB API", description="HBnB Application API", doc="/api/v1/")
+    api = Api(
+        app,
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/"
+    )
 
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(auth_ns, path="/api/v1/auth")
