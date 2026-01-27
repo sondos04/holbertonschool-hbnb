bc0b8efe656d4551bd9b0629f4b9fb4c93ca7dce
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-27 16:34:43 +0300

    Refactor PlaceRepository to use Flask-SQLAlchemy

--- part3/hbnb/app/repositories/place_repository.py
+++ part3/hbnb/app/repositories/place_repository.py
@@ -1,12 +1,10 @@
 from app.repositories.sqlalchemy_repository import SQLAlchemyRepository
 from app.models.place import Place
 
+
 class PlaceRepository(SQLAlchemyRepository):
-	def __init__(self, session):
-		super().__init__(session, Place)
-	def get_by_owner(self, user_id):
-		return(
-		self.session.query(Place)
-		.filter(Place.owner_id == user_id)
-		.all()
-		)
+    def init(self):
+        super().init(Place)
+
+    def get_by_owner(self, user_id):
+        return Place.query.filter_by(owner_id=user_id).all()
