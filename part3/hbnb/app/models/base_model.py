95225fd02425c9c9ee3450d9427ff5e108a4cb3f
Nada Al-Mutairi <nada.ghannam19@gmail.com>
2026-01-27 16:14:15 +0300

    Refactor BaseModel to use SQLAlchemy ORM

--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -1,24 +1,10 @@
 import uuid
 from datetime import datetime
+from app.extensions import db
 
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.orm import declarative_base
+class BaseModel(db.Model):
+    __abstract__ = True
 
-Base = declarative_base()
-
-
-class BaseModel:
-    id = Column(
-        String(60),
-        primary_key=True,
-        default=lambda: str(uuid.uuid4())
-    )
-    created_at = Column(
-        DateTime,
-        default=datetime.utcnow
-    )
-    updated_at = Column(
-        DateTime,
-        default=datetime.utcnow,
-        onupdate=datetime.utcnow
-    )
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
