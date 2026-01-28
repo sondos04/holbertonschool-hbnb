192a9964a6931c7469c5a45defc7cc333fc32dd8
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-28 23:54:04 +0300

    Update run.py

--- part3/hbnb/run.py
+++ part3/hbnb/run.py
@@ -10,12 +10,20 @@ def get_config():
         return ProductionConfig
     return DevelopmentConfig
 
-
 app = create_app(get_config())
 
 with app.app_context():
+    from app.models.user import User
+    from app.models.place import Place
+    from app.models.review import Review
+    from app.models.amenity import Amenity
+
     db.create_all()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=app.config.get("DEBUG", False))
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=app.config.get("DEBUG", False)
+    )
