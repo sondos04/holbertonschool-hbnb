ba7b2e98088530636abea5c26b7ef067dd90da21
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-02-15 00:03:00 +0300

    Update amenity.py

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -6,17 +6,18 @@ class Amenity(BaseModel):
     __tablename__ = "amenities"
 
     name = db.Column(db.String(50), nullable=False, unique=True)
-
-    places = db.relationship(
-        "Place",
-        secondary=place_amenity,
-        back_populates="amenities"
-    )
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "created_at": self.created_at.isoformat() if self.created_at else None,
-            "updated_at": self.updated_at.isoformat() if self.updated_at else None,
-        }
+##
+##    places = db.relationship(
+##        "Place",
+##        secondary=place_amenity,
+##        back_populates="amenities"
+##    )
+##
+##    def to_dict(self):
+##        return {
+##            "id": self.id,
+##            "name": self.name,
+##            "created_at": self.created_at.isoformat() if self.created_at else None,
+##            "updated_at": self.updated_at.isoformat() if self.updated_at else None,
+##        }
+##
