b57fe5190766c3110e3f5c53a30fa27414aacf64
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-02-14 22:06:11 +0300

    Update config.py

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -1,9 +1,12 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = "super-secret-key"
     JWT_SECRET_KEY = "jwt-super-secret-key"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
+
 
 
 class DevelopmentConfig(Config):
