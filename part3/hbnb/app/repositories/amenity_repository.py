7abb41878f167ae64298e897957e59753e03df19
Shaden Alalwani <viuxwl7@gmail.com>
2026-02-14 22:15:47 +0300

    Update amenity_repository.py

--- part3/hbnb/app/repositories/amenity_repository.py
+++ part3/hbnb/app/repositories/amenity_repository.py
@@ -4,8 +4,8 @@ from app.models.amenity import Amenity
 
 
 class AmenityRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Amenity)
+    def init(self):
+        super().init(Amenity)
 
     def get_by_name(self, name):
         return self.model.query.filter_by(name=name).first()
